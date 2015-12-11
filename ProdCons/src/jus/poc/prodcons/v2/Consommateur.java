a9c76d672622dac00c082bbab5edd6d08e613f18
Elsa Navarro <elsa0navarro@gmail.com>
2015-12-11 16:37:19 +0100

    Push initial v2

---
+++ ProdCons/src/jus/poc/prodcons/v2/Consommateur.java
@@ -0,0 +1,59 @@
+package jus.poc.prodcons.v2;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Consommateur;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	private int nbMess = 0;
+	private ProdCons pc;
+
+	protected Consommateur(int type, Observateur observateur,
+			int moyenneTempsDeTraitement, int deviationTempsDeTraitement,
+			ProdCons pc, int nombreMoyenNbExemplaire,
+			int deviationNombreMoyenNbExemplaire) throws ControlException {
+		super(type, observateur, moyenneTempsDeTraitement,
+				deviationTempsDeTraitement);
+		this.nbMess = Aleatoire.valeur(nombreMoyenNbExemplaire,
+				deviationNombreMoyenNbExemplaire);
+		this.pc = pc;
+	}
+
+	@Override
+	public void run() {
+		int tAlea;
+		Message m = null;
+		while (nombreDeMessages() > 0) {
+			try {
+				m = this.pc.get(this);
+				System.out.println("Message consommé par " + identification()
+						+ ": " + m);
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			} catch (Exception e) {
+
+				e.printStackTrace();
+			}
+
+			tAlea = new Aleatoire(moyenneTempsDeTraitement,
+					deviationTempsDeTraitement).next();
+			try {
+				Thread.sleep(tAlea);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			this.nbMess--;
+		}
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		return this.nbMess;
+	}
+
+}
---
+++ ProdCons/src/jus/poc/prodcons/v2/Producteur.java
@@ -0,0 +1,57 @@
+package jus.poc.prodcons.v2;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Producteur;
+
+public class Producteur extends Acteur implements _Producteur {
+
+	private int nbMess = 0;
+	private ProdCons pc;
+
+	protected Producteur(int type, Observateur observateur,
+			int moyenneTempsDeTraitement, int deviationTempsDeTraitement,
+			int nombreMoyenNbExemplaire, int deviationNombreMoyenNbExemplaire,
+			ProdCons pc) throws ControlException {
+		super(type, observateur, moyenneTempsDeTraitement,
+				deviationTempsDeTraitement);
+		this.nbMess = new Aleatoire(nombreMoyenNbExemplaire,
+				deviationNombreMoyenNbExemplaire).next();
+		this.pc = pc;
+	}
+
+	@Override
+	public void run() {
+		int messNum = 1;
+		int tAlea;
+		while (nbMess > 0) {
+			tAlea = Aleatoire.valeur(moyenneTempsDeTraitement(),
+					deviationTempsDeTraitement());
+			try {
+				Thread.sleep(tAlea);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Message m = new MessageX(super.identification(),
+					", je suis le message numéro " + messNum);
+			try {
+				this.pc.put(this, m);
+				System.out.println(m);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			this.nbMess--;
+			messNum++;
+		}
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		return this.nbMess;
+	}
+}
