e78b3f6a3d37beb0331862e91a8fba2317f20ec6
Elsa Navarro <elsa0navarro@gmail.com>
2015-12-15 18:23:19 +0100

    début des tests + modif nom variable paramètre dans la creation du producteur dans TestProdCons

--- ProdCons/src/jus/poc/prodcons/v1/TestProdCons.java
+++ ProdCons/src/jus/poc/prodcons/v1/TestProdCons.java
@@ -11,6 +11,8 @@ import jus.poc.prodcons.Simulateur;
 
 public class TestProdCons extends Simulateur {
 
+	static String fichierTest;
+
 	int nbProd;
 	int nbCons;
 	int nbBuffer;
@@ -34,44 +36,52 @@ public class TestProdCons extends Simulateur {
 	@Override
 	protected void run() throws Exception {
 		// Corps du programme principal
-		this.init("jus/poc/prodcons/options/v1.xml");// On lit un xml
-		ProdCons pc = new ProdCons(nbBuffer);
-		Producteur[] prods = new Producteur[nbProd];// Tableau des producteurs
-		Consommateur[] cons = new Consommateur[nbCons];// Tableau des
-														// consommateurs
-		/* On crée les producteurs */
-		LOGGER.info("Je crée les producteurs");
-		LOGGER.info("Nombre: " + nbProd);
+		this.init("jus/poc/prodcons/options/" + fichierTest);// On lit un xml
+		if ((nbProd > 0) && (nbCons > 0)) {
+			ProdCons pc = new ProdCons(nbBuffer);
+			Producteur[] prods = new Producteur[nbProd];// Tableau des
+														// producteurs
+			Consommateur[] cons = new Consommateur[nbCons];// Tableau des
+															// consommateurs
+			/* On crée les producteurs */
+			LOGGER.info("Je crée les producteurs");
+			LOGGER.info("Nombre: " + nbProd);
 
-		for (int i = 0; i < prods.length; i++) {
-			prods[i] = new Producteur(1, observateur, tempsMoyenProduction,
-					deviationTempsMoyenProduction, nombreMoyenNbExemplaire,
-					deviationNombreMoyenNbExemplaire, pc);// Creation prods
-			prods[i].start();
-		}
+			for (int i = 0; i < prods.length; i++) {
+				prods[i] = new Producteur(1, observateur, tempsMoyenProduction,
+						deviationTempsMoyenProduction, nombreMoyenDeProduction,
+						deviationNombreMoyenDeProduction, pc);// Creation prods
+				prods[i].start();
+			}
 
-		LOGGER.info("Je crée les consommateurs");
-		LOGGER.info("Nombre: " + nbCons);
+			LOGGER.info("Je crée les consommateurs");
+			LOGGER.info("Nombre: " + nbCons);
 
-		for (int i = 0; i < cons.length; i++) {
-			cons[i] = new Consommateur(2, observateur, tempsMoyenConsommation,
-					deviationTempsMoyenConsommation, pc,
-					nombreMoyenNbExemplaire, deviationNombreMoyenNbExemplaire); // Creation
-			// Cons
-			cons[i].start();
+			for (int i = 0; i < cons.length; i++) {
+				cons[i] = new Consommateur(2, observateur,
+						tempsMoyenConsommation,
+						deviationTempsMoyenConsommation, pc,
+						nombreMoyenDeProduction,
+						deviationNombreMoyenDeProduction); // Creation
+				// Cons
+				cons[i].start();
+			}
+			/* Vérification fin exécution pour terminer appli */
+			for (int i = 0; i < prods.length; i++) {
+				prods[i].join();
+			}
+			do {
+				Thread.sleep(250);
+			} while (pc.enAttente() > 0);
+			LOGGER.info("Simulation terminée.");
+		} else {
+			LOGGER.info("Simulation annulée : abscence de producteur ou de consommateur.");
 		}
-		/* Vérification fin exécution pour terminer appli */
-		for (int i = 0; i < prods.length; i++) {
-			prods[i].join();
-		}
-		do {
-			Thread.sleep(250);
-		} while (pc.enAttente() > 0);
-		LOGGER.info("Simulation terminée.");
 		System.exit(0);
 	}
 
 	public static void main(String[] args) {
+		fichierTest = args[0];
 		new TestProdCons(new Observateur()).start();
 	}
 
